64756872665484c9e5b0392c63ae0928376f2da9
Andriy03092001 <cuanid236316@gmail.com>
2020-09-14 18:21:15 +0300

    first

---
+++ NewsAPI/Controllers/NewsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using News.DataAccess;
+using News.DataAccess.Entity;
+using NewsAPI.Models;
+
+namespace NewsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/News")]
+    public class NewsController : Controller
+    {
+        private readonly EFContext _context;
+        public NewsController(EFContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<NewsViewModel> getNews()
+        {
+            List<NewsViewModel> data = _context.News.Select(t => new NewsViewModel()
+            {
+                Id = t.Id,
+                DatePost = t.DatePost,
+                Description = t.Description,
+                LinkImage = t.LinkImage,
+                Title = t.Title
+            }).ToList();
+
+            return data;
+        }
+
+        [HttpPost("postNews")]
+        public ResultDTO postNews([FromBody]NewsCreateDTO model)
+        {
+            try
+            {
+                tblNews newNews = new tblNews() { 
+                    DatePost = model.DatePost,
+                    Description = model.Description,
+                    LinkImage = model.LinkImage,
+                    Title = model.Title
+                };
+
+                _context.News.Add(newNews);
+                _context.SaveChanges();
+
+                return new ResultDTO
+                {
+                    Status = 200,
+                    Message = "OK"
+                };
+            }
+            catch (Exception e)
+            {
+                return new ResultDTO
+                {
+                    Status = 500,
+                    Message = e.Message
+                };
+            }
+
+
+        }
+
+
+    }
+}
---
+++ NewsAPI/Models/NewsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsAPI.Models
+{
+    public class NewsViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string DatePost { get; set; }
+        public string LinkImage { get; set; }
+        public string Description { get; set; }
+    }
+}
